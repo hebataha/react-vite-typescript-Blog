095849e62b6a5259f6c2ef7054340427ddefe29b
hebataha <AMLTAHA866@gmail.com>
2023-06-10 00:36:02 +0200

    add-localization

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import styles from "./style.module.scss";
+import { trans } from "@mongez/localization";
 import { Link } from "@mongez/react-router";
 const Header: React.FC = () => {
   return (
+  
     <div className={styles.header}>
+       
       <div className={styles.logo}>LOGO</div>
       <nav>
         <ul>
           <li>
-            <Link to="/home">Home</Link>
+            <Link to="/home"> {trans("home")}</Link>
           </li>
           <li>
             <Link to="/about">About</Link>
