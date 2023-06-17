62dc022822b6fa13926280fa3914b9c6b37e9d04
hebataha <AMLTAHA866@gmail.com>
2023-06-17 20:06:50 +0200

    add TSX file in header and footer components

--- src/components/shared/Index.tsx
+++ src/components/shared/Index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './Header/Header'
-import Footer from './Footer/Footer'
+import Header from './Header'
+import Footer from './Footer'
 
 export default function IndexShared() {
 function getPara (paraData){
